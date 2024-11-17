77baaa213359257623b771b0a12a72f7e67159a5
Robson D´Ávila <mrfdavila@gmail.com>
2024-11-16 21:53:37 -0300

    Commit inicial da API Pulseira - GrafCarlos

---
+++ src/main/java/com/grafcarlos/api_pulseira/ApiPulseiraApplication.java
@@ -0,0 +1,13 @@
+package com.grafcarlos.api_pulseira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPulseiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPulseiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grafcarlos/api_pulseira/ApiPulseiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grafcarlos.api_pulseira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPulseiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
